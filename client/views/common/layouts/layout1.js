2410d717b593507dfbe280220d78c497a425e69e
Alexis Le Boulc'h <alexisleboulch@gmail.com>
2016-12-06 14:25:03 +0100

    menu actif
    update 05/12/2016

--- client/views/common/layouts/layout1.js
+++ client/views/common/layouts/layout1.js
@@ -2,3 +2,81 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './layout1.html';
+
+//notifications dropdown function
+$(function() {
+  // Setup drop down menu
+  $('.dropdown-toggle').dropdown();
+ 
+  // Fix input element click problem
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+});
+
+//match hammer redirect
+Template.layout1.events({
+  'click #navbarMatchmakingLink': function(e) {
+    e.preventDefault();
+    Router.go('matchmaking');
+  }
+});
+//menu open modal
+Template.layout1.events({
+  'click #menu': function(e) {
+    e.preventDefault();
+    $('#menuModal').modal('show');
+  }
+});
+
+////////////////////////////////////////
+//menu template change view view links//
+////////////////////////////////////////
+Template.menuModal.events({
+    'click #menuMatchmakingLink': function(e, t){
+      console.log("You clicked");
+      console.log(this);
+       /*Meteor.Router.to('/rooms/' + this.id);*/
+       $('#menuModal').modal('hide');
+       Router.to('/matchmaking');
+
+     }
+});
+     Template.menuModal.events({
+     'click #menuGamesManagerLink': function(e, t){
+      console.log("You clicked");
+      console.log(this);
+       $('#menuModal').modal('hide');
+       Router.go('/gamesManager');
+     }
+});
+     Template.menuModal.events({
+     'click #menuConversationsLink': function(e, t){
+      console.log("You clicked");
+      console.log(this);
+      $('#menuModal').modal('hide');
+       Router.go('/conversations');
+     }
+});
+     Template.menuModal.events({
+     'click #menuFriendsListLink': function(e, t){
+      console.log("You clicked");
+      console.log(this);
+      $('#menuModal').modal('hide');
+       Router.go('/friendsList');
+     }
+});
+     Template.menuModal.events({
+     'click #menuProfileLink': function(e, t){
+      console.log("You clicked");
+      console.log(this);
+      $('#menuModal').modal('hide');
+       Router.go('/profile');
+     }
+});
+//close dialogue onClick
+/*     'click .menuLinks': function(e, t){
+      console.log("You clicked");
+      console.log(this);
+      $('#menuModal').modal('hide');
+     }*/
--- client/views/pages/matchmaking.js
+++ client/views/pages/matchmaking.js
@@ -2,3 +2,22 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './matchmaking.html';
+
+/*  Template.matchmaking.rendered = function (){
+    var handle = $( "#custom-handle" );
+    $( "#sliderDistance" ).slider({
+      create: function() {
+        handle.text( $( this ).slider( "value" ) );
+      },
+      slide: function( event, ui ) {
+        handle.text( ui.value );
+      }
+    });
+  };*/
+  $('#ex1').slider({
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
+
+$("#ex2").slider({});
