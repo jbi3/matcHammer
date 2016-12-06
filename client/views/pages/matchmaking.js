617161518f8fd97a764d40a17162125fb47d803a
Alexis Le Boulc'h <alexisleboulch@gmail.com>
2016-12-06 20:19:27 +0100

    update router fonctionnel + matchmaking noUislider test
    tout est dans le titre

--- client/views/pages/matchmaking.js
+++ client/views/pages/matchmaking.js
@@ -1,9 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
 import './matchmaking.html';
 
-/*  Template.matchmaking.rendered = function (){
+/*  Template.matchmaking.event = function (){
     var handle = $( "#custom-handle" );
     $( "#sliderDistance" ).slider({
       create: function() {
@@ -14,10 +13,39 @@ import './matchmaking.html';
       }
     });
   };*/
-  $('#ex1').slider({
-  formatter: function(value) {
-    return 'Current value: ' + value;
+
+var stepSlider = document.getElementById('slider-step');
+
+noUiSlider.create(stepSlider, {
+  start: [ 4000 ],
+  step: 1000,
+  range: {
+    'min': [  2000 ],
+    'max': [ 10000 ]
   }
 });
 
-$("#ex2").slider({});
+var stepSliderValueElement = document.getElementById('slider-step-value');
+
+stepSlider.noUiSlider.on('update', function( values, handle ) {
+  stepSliderValueElement.innerHTML = values[handle];
+});
+
+Template.points2PlayWith.rendered = function() {
+    $('.slider').each(function() {
+      var $this = $(this);
+      $this.noUiSlider({
+        start: 5,
+        connect: "lower",
+        step: 0,
+        format: wNumb({
+          decimals: 0,
+        }),
+        range: {
+          'min': 0,
+          'max': 10
+        }
+      });
+      $this.Link('lower').to($('.range'));
+    });
+  };
