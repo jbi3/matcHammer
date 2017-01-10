e385ec5387e4795bf6e4a615cde05d4a84a7818d
Alexis Le Boulc'h <alexisleboulch@gmail.com>
2017-01-10 18:03:04 +0100

    hophophop

--- client/views/pages/matchmaking.js
+++ client/views/pages/matchmaking.js
@@ -5,7 +5,15 @@ import './matchmaking.html';
 /*Template.matchmaking("currentFieldValue", function (fieldName) {
   return AutoForm.getFieldValue("reactiveCurrentValueForm", fieldName) || "not selected";
 });*/
-AutoForm.hooks({
+var sliderValue = salut.value;
+Template.matchmaking.rangeValue=function(){
+     
+     return sliderValue;
+     //then you can get this session and return it in a helper to display on your page
+};
+
+
+/*AutoForm.hooks({
   matchMaker: {
     onSubmit: function (insertDoc, updateDoc, currentDoc) {
       
@@ -17,4 +25,4 @@ AutoForm.hooks({
       return false;
     }
   }
-});
+});*/
