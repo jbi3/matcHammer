7cc41e9885768b47974acd2edebbe896f18ea15d
Alexis Le Boulc'h <alexisleboulch@gmail.com>
2017-01-10 16:13:18 +0100

    up matchmaking sliders

--- client/views/pages/matchmaking.js
+++ client/views/pages/matchmaking.js
@@ -5,3 +5,16 @@ import './matchmaking.html';
 /*Template.matchmaking("currentFieldValue", function (fieldName) {
   return AutoForm.getFieldValue("reactiveCurrentValueForm", fieldName) || "not selected";
 });*/
+AutoForm.hooks({
+  matchMaker: {
+    onSubmit: function (insertDoc, updateDoc, currentDoc) {
+      
+      if (customHandler(insertDoc)) {
+        this.done(console.log("submission succeded"));
+      } else {
+        this.done(new Error("Submission failed"));
+      }
+      return false;
+    }
+  }
+});
