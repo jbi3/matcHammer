3872a7723c31474de1b16d32e4f18fc9feffb2aa
Alexis Le Boulc'h <alexisleboulch@gmail.com>
2017-01-17 16:32:02 +0100

    essai avancement games manager

--- client/views/pages/gamesManager.js
+++ client/views/pages/gamesManager.js
@@ -2,3 +2,10 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './gamesManager.html';
+
+/*var userLogged = Meteor.user()
+Template.gamesWaitingList.helper({
+	MatchmakingsList: function() {
+  return Matchmakings.find();
+}
+});*/
