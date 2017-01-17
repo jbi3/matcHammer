3298115522e4e180edfce607c19245e7d0029810
Alexis Le Boulc'h <alexisleboulch@gmail.com>
2017-01-17 18:40:29 +0100

    gamesManager basics
    matchmaking synchronisé au gamesManager

--- client/views/pages/gamesManager.js
+++ client/views/pages/gamesManager.js
@@ -3,9 +3,11 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './gamesManager.html';
 
-/*var userLogged = Meteor.user()
-Template.gamesWaitingList.helper({
-	MatchmakingsList: function() {
+/*var userLogged = Meteor.user()*/
+var playerId = this._id;
+Template.gamesWaitingList.helpers ({
+
+	'match': function() {
   return Matchmakings.find();
 }
-});*/
+});
--- client/views/pages/profile.js
+++ client/views/pages/profile.js
@@ -2,3 +2,9 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './profile.html';
+
+Template.profile.helpers({
+  Name: function() {
+    return Meteor.user().profile.username;
+  }
+});
