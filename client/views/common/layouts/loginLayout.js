fb70106890320c3ee8746a324e369beed5a5ea20
jbi3 <jean.baptiste.barbera@gmail.com>
2017-02-10 16:10:29 +0100

    preloader CSS + accueil
    ajout preloader CSS
    + style page accueil

--- client/views/common/layouts/loginLayout.js
+++ client/views/common/layouts/loginLayout.js
@@ -2,3 +2,11 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './loginLayout.html';
+
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+    }, 3000);
+ 
+});
